498246f3d1626362e0bcf57a819ed4d08ea41dc9
Welle Charlotte <charlotte.welle@absi.be>
2017-06-21 12:19:58 +0200

    Changes:
    - Add aspect ratio

--- JMetadata/javasource/aka/jmetadata/main/constants/video/AspectRatio.java
+++ JMetadata/javasource/aka/jmetadata/main/constants/video/AspectRatio.java
@@ -10,52 +10,72 @@ public enum AspectRatio {
     /**
      * Unknown
      */
-    UNKOWN("UNKOWN", 0),
+    UNKOWN("UNKOWN", 0, "UNKOWN"),
 
     /**
-     * 1:33
+     * 5/4 (1.25:1)
      */
-    AS_1_33("1.33", 1.33),
+    AS_1_25("1.25", 1.25, "5/4"),
 
     /**
-     * 1:66
+     * 4/3 (1.33:1)
      */
-    AS_1_66("1.66", 1.66),
+    AS_1_333("1.333", 1.333, "4/3"),
 
     /**
-     * 1:78
+     * IMAX (1.43:1)
      */
-    AS_1_78("1.78", 1.78),
+    AS_1_43("1.43", 1.43, "IMAX"),
 
     /**
-     * 1:85
+     * 5/3 (1.66:1)
      */
-    AS_1_85("1.85", 1.85),
+    AS_1_66("1.66", 1.66, "5/3"),
 
     /**
-     * 2:20
+     * 16/9 (1.77:1)
      */
-    AS_2_20("2.20", 2.20),
+    AS_1_778("1.778", 1.778, "16/9"),
 
     /**
-     * 2:35
+     * 35 mm US and UK widescreen standard for theatrical film (1.85:1)
      */
-    AS_2_35("2.35", 2.35);
+    AS_1_85("1.85", 1.85, "35 mm US and UK widescreen standard for theatrical film"),
+
+    /**
+     * 11/5 (2.20:1)
+     */
+    AS_2_20("2.20", 2.20, "11/5"),
+
+    /**
+     * 21/9 (2.35:1)
+     */
+    AS_2_35("2.35", 2.35, "21/9");
 
     private @NonNull String aspectRationName;
+    private @NonNull String fullName;
     private double aspectRatio;
 
-    private AspectRatio(@NonNull final String aspectRationName, final double aspectRatio) {
+    private AspectRatio(@NonNull final String aspectRationName, final double aspectRatio, @NonNull final String fullName) {
         this.aspectRationName = aspectRationName;
+        this.fullName = fullName;
         this.aspectRatio = aspectRatio;
     }
 
     /**
-     * @return the aspect ration
+     * @return the aspect ratio
      */
     @NonNull
     public final String getAspectRatioName() {
         return this.aspectRationName;
+    }
+
+    /**
+     * @return the aspect ratio full name
+     */
+    @NonNull
+    public final String getAspectRatioFullName() {
+        return this.fullName;
     }
 
     /**
