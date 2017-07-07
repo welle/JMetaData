fa8dbab8c02ab24fc44187281c3872f4361a5ebb
Welle Charlotte <charlotte.welle@absi.be>
2017-07-07 14:33:47 +0200

    corrections

--- Test/javasource/aka/jmetadata/test/JMetaDataAudio_Test.java
+++ Test/javasource/aka/jmetadata/test/JMetaDataAudio_Test.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.management.RuntimeErrorException;
+
 import org.eclipse.jdt.annotation.NonNull;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -46,9 +48,12 @@ public final class JMetaDataAudio_Test {
                 if (audioStreams.get(0) != null) {
                     jMetaDataAudio = audioStreams.get(0);
                 }
+            } else {
+                throw new RuntimeErrorException(null, "Can not open file.");
             }
         } catch (final Throwable e) {
             LOGGER.log(Level.SEVERE, e.getMessage());
+            throw new RuntimeErrorException(null, "Can not open file.");
         }
     }
 
